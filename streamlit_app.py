0344e320c2d2bf4d7ba72832f27a8cd93876bcc4
smett9690 <119883513+smett9690@users.noreply.github.com>
2024-11-21 11:08:52 -0500

    Update streamlit_app.py
    Removed the dataframe container visibility before the ingredients list in the SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data = my_dataframe, use_container_width=True)
+# st.dataframe(data = my_dataframe, use_container_width=True)
 
 
 ingredients_list = st.multiselect(
