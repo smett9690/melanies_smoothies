b4da1dc5a01029d9291a8c44362bb94c6a37ebdd
smett9690 <119883513+smett9690@users.noreply.github.com>
2024-11-21 11:12:34 -0500

    Update streamlit_app.py
    Incorrect version copied from the snowflake streamlit app. 
    made change:
    added:
    name on order and the success message

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,10 @@ st.write(
     """Choose the fruits you want in your current smoothie
     """
 )
+
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your Smoothie will be: ", name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -15,7 +19,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 
 ingredients_list = st.multiselect(
-    "Choose up to 5 ingredients", my_dataframe
+    "Choose up to 5 ingredients", my_dataframe, max_selections = 5
     
 )
 if ingredients_list:
@@ -26,17 +30,17 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        my_insert_stmt = """insert into smoothies.public.orders(ingredients)
-        values('"""+ ingredients_string +"""')
+        my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order)
+        values('"""+ ingredients_string + """','"""+name_on_order+"""')
         """
 
     #st.write(my_insert_stmt)
+    #st.stop()
 
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
-
+        st.success("""Your Smoothie is ordered,"""+name_on_order+"""!""", icon="✅")
 
     
